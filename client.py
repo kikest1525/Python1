d576ac78a70cc30715cb876c00f1063b2a6faeed
Simeon Wuthier <simeonwuthier@gmail.com>
2022-07-19 19:42:20 -0600

    Fix the source

--- client.py
+++ client.py
@@ -1,31 +1,46 @@
 import socket
 import threading
+import signal
+import sys
 
-nickname = input ("choose a nickname:")
+name = input('Please choose a name: ')
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
 
-def receive():
-    while True:
-        try:
-            message= client.recv(1024).decode('ascii')
-            if message -- 'NICK':
-                client.send(nickname.encode('ascii'))
-            else:
-                print(message)
-        except:
-            print("An error occurred")
-            client.close()
-            break
-
+# Thread that writes new messages
 def write():
-    while True:
-        message= f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
+	while True:
+		message = input()
+		client.send(message.encode('ascii'))
 
-        receive_thread - threading.Thread(target-receive)
-        receive_thread.start()
+# Thread that receives incoming messages
+def receive():
+	while True:
+		try:
+			message = client.recv(1024).decode('ascii')
 
-        write_thread = threading.Thread(target=write)
-        write_thread.start()
+			if message == 'GETNICKNAME':
+				client.send(name.encode('ascii'))
+			else:
+				print(message)
+		except:
+			print('An error occurred')
+			client.close()
+			break
+
+# Handle the user pressing CTRL+C to exit
+def signal_handler(sig, frame):
+	print('You pressed Ctrl+C!')
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+# Start the thread that listens for messages
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+# Start the thread that writes new messages
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- server.py
+++ server.py
@@ -1,50 +1,65 @@
 import threading
 import socket
+import signal
+import sys
 
-host= '127.0.0.1' # localhost
-port= 55555
+host = '127.0.0.1'  # localhost
+port = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
 clients = []
-nicknames = []
+names = []
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
 
-def handle(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index + clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname - nicknames[index]
-            broadcast(f'{nickname} left the caht'.encode('ascii'))
-            nicknames.remove(nickname)
-            break
+# Broadcast a message to every user
+def broadcast(name, message):
+	for client in clients:
+		client.send(name.encode('ascii') + message)
 
+# Each user gets an individual thread
+def listenToUser(client):
+	index = clients.index(client)
+	name = names[index]
+	while True:
+		try:
+			message = client.recv(1024)
+			broadcast(name, message)
+		except:
+			index = clients.index(client)
+			clients.remove(client)
+			client.close()
+			name = names[index]
+			broadcast(f'{name} left the chat'.encode('ascii'))
+			names.remove(name)
+			broadcast(f'Users online: [{", ".join(names)}]'.encode('ascii'))
+			break
+
+# Listen for incoming connection requests
 def receive():
-    while True:
-        client, address = server.accept()
-        print(f"connected with {str(address)} ")
+	while True:
+		client, address = server.accept()
+		print(f'Connected with {str(address)}')
+		client.send('GETNICKNAME'.encode('ascii'))
+		name = client.recv(1024).decode('ascii')
+		names.append(name)
+		clients.append(client)
 
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        nicknames.append(client)
+		broadcast(f'User joined the chat: {name}'.encode('ascii'))
+		client.send('Connected to the server!'.encode('ascii'))
 
-        print(f'nickname of the client is {nickname}!')
-        broadcast(f'{nickname}joined the caht!'. encode('ascii'))
-        client.send('connected to the server!'.encode('ascii'))
+		# Upon successfully connecting, set a thread that listens to that user
+		thread = threading.Thread(target=listenToUser, args=(client,))
+		thread.start()
 
-        thread - threading.Thread(target-handle, args-(client,))
-        thread.start()
+# Handle the user pressing CTRL+C to exit
+def signal_handler(sig, frame):
+	print('You pressed Ctrl+C!')
+	sys.exit(0)
 
-print("server is listening...")
+signal.signal(signal.SIGINT, signal_handler)
+
+print('Server is listening...')
 receive()
